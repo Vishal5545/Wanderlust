82d8221ad3612231511da1bc87bc3b6efb0b34e6
Vishal Bhoir <vgb8770@gmail.com>
2025-01-16 15:12:49 +0530

    error is solve

--- app.js
+++ app.js
@@ -72,12 +72,12 @@ const sessionOpations = {
 app.use(session(sessionOpations));
 app.use(flash());
 
-app.use(passport.initialize()); ///
-app.use(passport.session()); ///
-passport.use(new LocalStrategy(User.authenticate())); ///
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+passport.use(new LocalStrategy(User.authenticate())); 
 
-passport.serializeUser(User.serializeUser()); ///
-passport.deserializeUser(User.deserializeUser()); ///
+passport.serializeUser(User.serializeUser()); 
+passport.deserializeUser(User.deserializeUser()); 
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
